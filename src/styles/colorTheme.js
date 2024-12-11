cf310af3115f1f641bc0b511b050b79c2a3d3110
hlk1m <gywjd5126@naver.com>
2024-12-11 15:52:35 +0900

    setup

---
+++ src/styles/colorTheme.js
@@ -0,0 +1,14 @@
+// Color Guide
+
+export const colorTheme = {
+  navy: "hsl(213, 96%, 18%)",
+  blue: "hsl(243, 100%, 62%)",
+  pastelBlue: "hsl(228, 100%, 84%)",
+  lightBlue: "hsl(206, 94%, 87%)",
+  red: "hsl(354, 84%, 57%)",
+  gray: "hsl(231, 11%, 63%)",
+  lightGray: "hsl(229, 24%, 87%)",
+  veryLightGray: "hsl(217, 100%, 97%)",
+  ivory: "hsl(231, 100%, 99%)",
+  white: "hsl(0, 0%, 100%)",
+};
